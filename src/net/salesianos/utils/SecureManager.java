d1c107a6c3aad417fc61f17abe5f2531b2cb2091
hortuadev <hortuadev@gmail.com>
2026-04-29 20:42:07 +0100

    feat(secure): add constructors for key generation and key reception

--- src/net/salesianos/utils/SecureManager.java
+++ src/net/salesianos/utils/SecureManager.java
@@ -13,6 +13,18 @@ public class SecureManager {
     private final Cipher decipher;
     private final SecretKey secretKey;
 
+    public SecureManager() {
+        this.secretKey = generateKey();
+        this.cipher = initCipher(Cipher.ENCRYPT_MODE, secretKey);
+        this.decipher = initCipher(Cipher.DECRYPT_MODE, secretKey);
+    }
+
+    public SecureManager(byte[] keyBytes) {
+        this.secretKey = new SecretKeySpec(keyBytes, ALGORITHM);
+        this.cipher = initCipher(Cipher.ENCRYPT_MODE, secretKey);
+        this.decipher = initCipher(Cipher.DECRYPT_MODE, secretKey);
+    }
+
     private SecretKey generateKey() {
         try {
             KeyGenerator kg = KeyGenerator.getInstance(ALGORITHM);
@@ -20,6 +32,16 @@ public class SecureManager {
             return kg.generateKey();
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException("No se pudo generar la clave AES", e);
+        }
+    }
+
+    private Cipher initCipher(int mode, SecretKey key) {
+        try {
+            Cipher c = Cipher.getInstance(ALGORITHM);
+            c.init(mode, key);
+            return c;
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException e) {
+            throw new RuntimeException("No se pudo inicializar el cifrador AES", e);
         }
     }
 
