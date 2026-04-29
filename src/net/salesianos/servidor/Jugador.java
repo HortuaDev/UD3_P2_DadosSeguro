619c43f57e06bbfc8e24ad1161432b5958a5226a
hortuadev <hortuadev@gmail.com>
2026-04-29 20:47:52 +0100

    feat(server): add encrypted enviar and recibirMensaje methods to Jugador

--- src/net/salesianos/servidor/Jugador.java
+++ src/net/salesianos/servidor/Jugador.java
@@ -52,4 +52,24 @@ public class Jugador {
     public void sumarPuntos(int puntos) {
         this.puntuacion += puntos;
     }
+
+    public void enviar(String mensaje) {
+        try {
+            byte[] cipherBytes = secure.encrypt(mensaje);
+            out.writeInt(cipherBytes.length);
+            out.write(cipherBytes);
+            out.flush();
+        } catch (IOException e) {
+            System.err.println("Error enviando mensaje a " + nombre + ": " + e.getMessage());
+        }
+    }
+
+    public String recibirMensaje() throws IOException {
+        int length = in.readInt();
+        if (length <= 0)
+            return null;
+        byte[] cipherBytes = new byte[length];
+        in.readFully(cipherBytes);
+        return secure.decrypt(cipherBytes);
+    }
 }
